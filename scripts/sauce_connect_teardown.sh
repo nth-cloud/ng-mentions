bbd26d45c1e5f9d1655e1b4e990abb4615a0a47c
trickeyone <joshua.en@gmail.com>
2020-07-27 20:45:57 -0700

    chore(CI): troubleshooting travis-ci sauceconnect

--- scripts/sauce_connect_teardown.sh
+++ scripts/sauce_connect_teardown.sh
@@ -2,6 +2,9 @@
 
 set -e -o pipefail
 
+CONNECT_LOG="$LOGS_DIR/sauce-connect"
+CONNECT_STDOUT="$LOGS_DIR/sauce-connect.stdout"
+CONNECT_STDERR="$LOGS_DIR/sauce-connect.stderr"
 
 echo "Shutting down Sauce Connect tunnel"
 
@@ -14,3 +17,7 @@ done
 
 echo ""
 echo "Sauce Connect tunnel has been shut down"
+
+[[ -f "$CONNECT_LOG" ]] && [[ -s "$CONNECT_LOG" ]] && echo "Sauce Connect Log:" && cat "$CONNECT_LOG";
+[[ -f "$CONNECT_STDOUT" ]] && [[ -s "$CONNECT_STDOUT" ]] && echo "Sauce Connect Stdout Log:" && cat "$CONNECT_STDOUT";
+[[ -f "$CONNECT_STDERR" ]] && [[ -s "$CONNECT_STDERR" ]] && echo "Sauce Connect Stderr Log:" && cat "$CONNECT_STDERR";
