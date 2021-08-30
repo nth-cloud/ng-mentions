5b1ddc0e8fda2fecdba9f109592f9ea2ffb136a1
trickeyone <joshua.en@gmail.com>
2021-08-29 22:22:57 -0500

    chore: adding schematics to build process

--- misc/copy-static-files.ts
+++ misc/copy-static-files.ts
@@ -1,7 +1,22 @@
-import {copyFileSync} from 'fs';
+import {copyFileSync, copySync, readJsonSync, writeJSONSync} from 'fs-extra';
 
 /**
  * Copies missing required static assets after the ng-bootstap build
  */
 
-['LICENSE', 'README.md'].forEach(file => copyFileSync(file, `dist/${file}`));
+const DEST = 'dist';
+const SCHEMATICS = `${DEST}/schematics`;
+
+// 1. Copy static assets
+['LICENSE', 'README.md'].forEach(file => copyFileSync(file, `${DEST}/${file}`));
+
+// 2. Copy built schematics to 'dist/ng-bootstrap'
+copySync('schematics/dist', SCHEMATICS);
+copyFileSync('schematics/collection.json', `${SCHEMATICS}/collection.json`);
+copyFileSync('schematics/ng-add/schema.json', `${SCHEMATICS}/ng-add/schema.json`);
+
+// 3. Patch version in the 'dist/package.json' with the one from 'package.json'
+const {version} = readJsonSync(`package.json`, {encoding: 'utf-8'});
+const packageJson = readJsonSync(`${DEST}/package.json`, {encoding: 'utf-8'});
+packageJson.version = version;
+writeJSONSync(`${DEST}/package.json`, packageJson, {spaces: 2, encoding: 'utf-8'});
